05e0fb21f90c489034e9e035929ebd139a58e2c5
solo yolo <solo@yolo.com>
2025-09-22 01:52:38 +0900

    vibe init backend and makefile

---
+++ App/Server/src/Server.Infrastructure/Data/AppDbContextExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Infrastructure.Data;
+
+public static class AppDbContextExtensions
+{
+  public static void AddApplicationDbContext(this IServiceCollection services, string connectionString) =>
+    services.AddDbContext<AppDbContext>(options =>
+         options.UseSqlite(connectionString));
+
+}
---
+++ App/Server/tests/Server.UnitTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System.Runtime.CompilerServices;
+global using Ardalis.SharedKernel;
+global using Server.Core.ContributorAggregate;
+global using Server.UseCases.Contributors.Create;
+global using Shouldly;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using NSubstitute;
+global using Xunit;
