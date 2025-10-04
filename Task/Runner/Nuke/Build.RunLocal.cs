3f1043d5ab099b64f1634dc29f4bc623951a96f5
solo yolo <solo@yolo.com>
2025-10-05 00:14:37 +0900

    refactor nuke build to split targets to separate files

---
+++ Task/Runner/Nuke/Build.RunLocal.cs
@@ -0,0 +1,27 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Npm;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.Npm.NpmTasks;
+
+public partial class Build
+{
+  Target RunLocalServer => _ => _
+    .Description("Run backend locally")
+    .Executes(() =>
+    {
+      DotNetRun(s => s.SetProjectFile(ServerProject));
+    });
+
+  Target RunLocalClient => _ => _
+    .Description("Run client locally")
+    .DependsOn(InstallClient)
+    .Executes(() =>
+    {
+      Console.WriteLine($"Starting Vite dev server in {ClientDirectory}");
+      NpmRun(s => s
+        .SetProcessWorkingDirectory(ClientDirectory)
+        .SetCommand("dev"));
+    });
+}
