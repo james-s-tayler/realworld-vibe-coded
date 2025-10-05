6a32893dc76f2004ede32f1960fab9c8db44f5a5
solo yolo <solo@yolo.com>
2025-10-05 22:41:45 +0900

    note about nuke

--- Task/Runner/Nuke/Build.cs
+++ Task/Runner/Nuke/Build.cs
@@ -2,5 +2,13 @@
 
 public partial class Build : NukeBuild
 {
-  public static int Main() => Execute<Build>();
+  public static int Main()
+  {
+    /*
+     * https://github.com/nuke-build/nuke/issues/1088
+     * When you try run two nuke builds in parallel, they conflict on the ~/.nuke/temp/build.log
+     */
+
+    return Execute<Build>();
+  }
 }
