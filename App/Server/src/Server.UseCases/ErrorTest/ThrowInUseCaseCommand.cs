8ca6dea0bd350bc68514cbb2934a8c811bb03d10
solo yolo <solo@yolo.com>
2025-10-23 02:01:57 +0900

    fix test failing due to mediatr pipeline behavior trying to connect to db

--- App/Server/src/Server.UseCases/ErrorTest/ThrowInUseCaseCommand.cs
+++ App/Server/src/Server.UseCases/ErrorTest/ThrowInUseCaseCommand.cs
@@ -1,3 +1,3 @@
 ﻿namespace Server.UseCases.ErrorTest;
 
-public record ThrowInUseCaseCommand() : ICommand<Result<string>>;
+public record ThrowInUseCaseQuery() : IQuery<Result<string>>;
--- App/Server/src/Server.UseCases/ErrorTest/ThrowInUseCaseHandler.cs
+++ App/Server/src/Server.UseCases/ErrorTest/ThrowInUseCaseHandler.cs
@@ -1,8 +1,8 @@
 ﻿namespace Server.UseCases.ErrorTest;
 
-public class ThrowInUseCaseHandler : ICommandHandler<ThrowInUseCaseCommand, Result<string>>
+public class ThrowInUseCaseHandler : IQueryHandler<ThrowInUseCaseQuery, Result<string>>
 {
-  public Task<Result<string>> Handle(ThrowInUseCaseCommand request, CancellationToken cancellationToken)
+  public Task<Result<string>> Handle(ThrowInUseCaseQuery request, CancellationToken cancellationToken)
   {
     throw new InvalidOperationException("This is a test exception thrown in the use case");
   }
